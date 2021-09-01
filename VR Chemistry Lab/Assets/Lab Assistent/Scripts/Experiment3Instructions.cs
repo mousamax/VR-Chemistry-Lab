9a106c1ee8506c0afd89ffa27f592dd39ee2153d
Ammar Yasser <ammaryasser.eng@gmail.com>
2021-09-01 16:12:10 +0200

    Added Experiment 3

--- VR Chemistry Lab/Assets/Lab Assistent/Scripts/Experiment3Instructions.cs
+++ VR Chemistry Lab/Assets/Lab Assistent/Scripts/Experiment3Instructions.cs
@@ -1,18 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Experiment3Instructions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text changingText;
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public RawImage wellDone;
+    public RawImage beakers;
+    public RawImage dixter;
+    AudioSource audioSource;
+    [SerializeField] AudioClip buttonSound;
+    [SerializeField] AudioClip instruction1Sound;
+    [SerializeField] AudioClip instruction2Sound;
+    [SerializeField] AudioClip finishingExperiment;
+    float delay = 1f;
+    public bool instruction1Done = false;
+    public bool instruction2Done = false;
+    public bool instruction3Done = false;
+
+
+    private void Start()
     {
-        
+        wellDone.gameObject.SetActive(false);
+        dixter.gameObject.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        TextChange();
+        if (instruction1Done == true)
+        {
+            //second instruction
+            instruction1Done = false;
+            Invoke("SecondInstruction", delay);
+        }
+        if (instruction2Done == true)
+        {
+            //third instruction
+            instruction2Done = false;
+            Invoke("Finish", delay);
+        }
     }
+
+    void ButtonClick()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(buttonSound);
+        btn1.gameObject.SetActive(false);
+        btn2.gameObject.SetActive(false);
+        btn3.gameObject.SetActive(false);
+        Invoke("FirstInstruction", delay);
+
+    }
+
+    public void TextChange()
+    {
+
+        btn3.onClick.AddListener(ButtonClick);
+    }
+    public void FirstInstruction()
+    {
+        changingText.text = "Put some hydrochloric acid in a flask.";
+        audioSource.Stop();
+        audioSource.PlayOneShot(instruction1Sound);
+        //instruction1Done = true;
+
+    }
+    public void SecondInstruction()
+    {
+        changingText.text = "Bring a piece of sodium and throw it in the flask, you will observe evaporating smoke and the liquid gets excited.";
+
+        audioSource.Stop();
+        //instruction2Done = true;
+        audioSource.PlayOneShot(instruction2Sound);
+
+    }
+    public void Finish()
+    {
+        changingText.gameObject.SetActive(false);
+        beakers.gameObject.SetActive(false);
+        wellDone.gameObject.SetActive(true);
+        dixter.gameObject.SetActive(true);
+        audioSource.Stop();
+        audioSource.PlayOneShot(finishingExperiment);
+    }
+
 }
--- VR Chemistry Lab/Assets/LiquidsPackage/ChemicalsData/ChemistryManager.cs
+++ VR Chemistry Lab/Assets/LiquidsPackage/ChemicalsData/ChemistryManager.cs
@@ -19,12 +19,18 @@ public class ChemistryManager : MonoBehaviour
     public bool Exp2;
     public bool Exp3;
 
+    bool Exp3Ins1FirstTime;
+    bool Exp3Ins2FirstTime;
+
     void Start()
     {
         HCL = new Chemicals("HydroChloric acid", "Blue", new UnityEngine.Color(0.54f, 0.792f, 0.73f), new UnityEngine.Color(0.651f, 0.980f, 1f), new UnityEngine.Color(0.247f, 0.557f, 0.6784f), 1.18f);
         KSCN = new Chemicals("Potassium Thiocyanate", "Blue", new UnityEngine.Color(0.54f, 0.792f, 0.73f), new UnityEngine.Color(0.651f, 0.980f, 1f), new UnityEngine.Color(0.247f, 0.557f, 0.6784f), 1.886f);
         FeCL3 = new Chemicals("Ferric Chloride", "Orange", new UnityEngine.Color(0.9803f, 0.894f, 0.44705f), new UnityEngine.Color(1, 0.843f, 0), new UnityEngine.Color(0.7725f, 0.2588f, 0), 2.9f);
         FeSCN3 = new Chemicals("Ferric thiocyanate", "Red", new UnityEngine.Color(0.54117f, 0.0117f, 0.0117f), new UnityEngine.Color(0.4f, 0f, 0f), new UnityEngine.Color(1f, 0.0117f, 0), 0.9487f);
+
+        Exp3Ins1FirstTime = true;
+        Exp3Ins2FirstTime = true;
     }
 
     // Update is called once per frame
@@ -37,6 +43,11 @@ public class ChemistryManager : MonoBehaviour
     {
         if (name == "HydroChloric acid")
         {
+            if(Exp3 && Exp3Ins1FirstTime)
+            {
+                Exp3Ins1FirstTime = false;
+                Canvas.GetComponent<Experiment3Instructions>().instruction1Done = true;
+            }
             LiquidContainers[index].GetComponent<LiquidBehavior>().Chem = HCL;
             LiquidContainers[index].GetComponent<LiquidBehavior>().AcquireLiquideProb();
         }
@@ -82,6 +93,11 @@ public class ChemistryManager : MonoBehaviour
     {
         if(chem == "HydroChloric acid")
         {
+            if (Exp3 && Exp3Ins2FirstTime)
+            {
+                Exp3Ins2FirstTime = false;
+                Canvas.GetComponent<Experiment3Instructions>().instruction2Done = true;
+            }
             return true;
         }
         return false;
@@ -90,5 +106,10 @@ public class ChemistryManager : MonoBehaviour
     public void setExp1()
     {
         Exp1 = true;
+    }
+
+    public void setExp3()
+    {
+        Exp3 = true;
     }
 }
